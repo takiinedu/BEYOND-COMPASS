3d8c23859800c30ac9c48a840e050f8bd0d16d38
takiindev <takin2k5@gmail.com>
2024-11-07 16:22:49 +0700

    up1

---
+++ public/scr/componencts/main.js
@@ -0,0 +1,11 @@
+const head = document.querySelector('head');
+head.innerHTML += `
+    <link rel="stylesheet" href="./scr/assets/css/header.css">
+    <link rel="stylesheet" href="./scr/assets/css/main.css">
+    <link rel="stylesheet" href="./scr/assets/css/font.css">
+`;
+document.addEventListener('DOMContentLoaded', () => {
+    const head = document.querySelector('body');
+    head.innerHTML += `
+    `;
+});
